294632c033bc8b55114fe5a41d758d466360f608
Paulo Filho <pcf.feat@gmail.com>
2023-02-15 21:02:53 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter-cep
+
+This is an app that searches for a location based on the input ZIP Code (Brazilian) via BuscaCep API.
---
+++ android/app/src/main/kotlin/com/example/flutter_cep/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_cep
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
